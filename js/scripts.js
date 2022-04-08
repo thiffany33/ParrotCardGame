b52c9e5ae5e87e302751dd0cd882824428783980
Thiffany <thiffanygabrielle48@gmail.com>
2022-04-09 00:02:01 +0100

    Finalizado filtro de numeros do prompt

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 getNumber();
 
+let numberCards;
+
 function getNumber() {
     numero = prompt("digite um numero par de 4 ate 14")
 
@@ -12,4 +14,18 @@ function getNumber() {
         alert("Insira um numero valido !")
         getNumber();
     }
+
+    if (numero > 14) {
+        alert("Insira um numero menor que 14");
+        getNumber();
+    }
+
+    if ((numero % 2) == 0) {
+        numberCards = number
+        generatedCards(numberCards);
+    }
+}
+
+function generatedCards(numCards) {
+
 }
